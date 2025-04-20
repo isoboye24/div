491937ea1f00a6bab2c36d09b6efc4bdad754c88
isoboye24 <isobarguy4real@gmail.com>
2025-04-20 18:09:48 +0200

    I added the interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,3 +1,6 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { StaticImageData } from 'next/image';
+
 export interface InputProps {
   label?: string;
   name: string;
@@ -16,4 +19,29 @@ export interface TextareaProps {
   placeholder?: string;
   required?: boolean;
   rows?: number;
+}
+
+export interface SocialMediaProps {
+  icon: IconProp;
+  size: number;
+  url: string;
+  bgColor?: string; // Tailwind class like 'bg-amber-500'
+}
+
+export interface PolygonPictureFrameProps {
+  src: string | StaticImageData;
+  sides?: number;
+  size?: number; // Tailwind sizing like "w-64 h-64"
+  borderColor?: string;
+  borderWidth?: number; // In pixels
+}
+
+export interface PictureFrameProps {
+  src: string | StaticImageData;
+  rotation?: string;
+  zIndex?: string;
+  position?: string;
+  size?: number;
+  translateX?: string;
+  translateY?: string;
 }
