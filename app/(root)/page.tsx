636545a241b864685b2be94891021046cc01abbc
isoboye24 <isobarguy4real@gmail.com>
2025-04-21 15:33:14 +0200

    I designed the home page.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import HomeTopSection from '@/components/ui/home/home-top-section';
+import AboutSection from '../../components/ui/home/about-section';
+import ProjectSection from '@/components/ui/home/project-section';
+import { Metadata } from 'next';
 
-const Home = () => {
-  return <div>Home</div>;
+export const metadata: Metadata = {
+  title: 'Home',
 };
 
-export default Home;
+const HomePage = () => {
+  return (
+    <div className="overflow-hidden">
+      <section className="">
+        <HomeTopSection />
+      </section>
+      <section className="py-10 xl:pt-25 xl:pb-40" id="about">
+        <AboutSection />
+      </section>
+      <section
+        className="py-10 sm:py-[20] xl:pt-25 xl:pb-40 bg-teal-600"
+        id="projects"
+      >
+        <ProjectSection />
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
