b1a287bfdeb459387455c8cc6ccbdd7f1f388fbb
isoboye24 <isobarguy4real@gmail.com>
2025-05-28 11:58:27 +0200

    I designed the view message page and also got the data.

--- components/ui/shared/view-contact-message.tsx
+++ components/ui/shared/view-contact-message.tsx
@@ -14,17 +14,24 @@ const ViewContactMessage = ({
         key={id}
         className="bg-white dark:bg-gray-700 shadow-md rounded-xl p-6 w-full mx-auto"
       >
-        <div className="grid grid-cols-3 gap-5 p-10">
+        <div className="grid grid-rows-3 gap-5 p-10">
           <div className="">
-            <h1 className="text-center mb-10">{`Message from ${message?.senderName}`}</h1>
+            <h1 className="text-center font-bold text-2xl">
+              Message from{' '}
+              <b className="italic text-teal-500">{message?.senderName}</b>
+            </h1>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mb-20">
-            <div className="">Email: {message?.senderEmail}</div>
-            <div className="">Subject: {message?.subject}</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-5 ">
+            <div className="">
+              Email: <b>{message?.senderEmail}</b>{' '}
+            </div>
+            <div className="">
+              Subject: <b>{message?.subject}</b>{' '}
+            </div>
           </div>
           <div className="">
-            <p className="">Message body</p>
-            <p className="">{message?.messageText}</p>
+            <p className="text-center text-xl font-semibold">Message body</p>
+            <p className="text-justify">{message?.messageText}</p>
           </div>
         </div>
       </div>
