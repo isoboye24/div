e9f0ec526a8b623d1cfb02db8688887f9a75471d
isoboye24 <isobarguy4real@gmail.com>
2026-01-30 00:49:08 +0100

    I bordered the project section

--- components/ui/home/project-section.tsx
+++ components/ui/home/project-section.tsx
@@ -5,7 +5,7 @@ import ProjectCarousel from '../project/project-carousel';
 
 const ProjectSection = () => {
   return (
-    <div className="wrapper">
+    <div className="wrapper border-0 lg:border-30 border-amber-200 border-">
       <div className="text-center max-w-2xl mx-auto">
         <h2 className="text-3xl md:text-4xl font-bold text-white inline-block">
           Projects
