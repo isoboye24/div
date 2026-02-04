469a332bd0acf673a66a0b8223f4a1f35af0d037
isoboye24 <isobarguy4real@gmail.com>
2026-02-04 11:58:49 +0100

    I change the logo in the header

--- components/ui/shared/header/index.tsx
+++ components/ui/shared/header/index.tsx
@@ -4,23 +4,39 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { APP_NAME } from '@/lib/constants';
 import { MainNav } from './navbar';
+import logoWhite from '@/public/images/logo-white.png';
+import logoDark from '@/public/images/logo-dark.png';
 
 const Header = () => {
   return (
     <header className="w-full ">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="ml-2 flex-start p-2">
+          <Link href="/" className="ml-2 flex-start">
             <Image
               priority={true}
-              src="/images/logo.png"
-              width={30}
-              height={30}
+              src={logoWhite}
+              width={50}
+              height={20}
               alt={`${APP_NAME} logo`}
+              className="block dark:hidden"
             />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
+            {/* <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
-            </span>
+            </span> */}
+          </Link>
+          <Link href="/" className="ml-2 flex-start">
+            <Image
+              priority={true}
+              src={logoDark}
+              width={50}
+              height={20}
+              alt={`${APP_NAME} logo`}
+              className="hidden dark:block"
+            />
+            {/* <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span> */}
           </Link>
         </div>
         <div className="hidden md:block">
